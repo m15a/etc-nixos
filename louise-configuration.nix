651e0ea50d95ac8308a771d293ca28a37a664206
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2018-03-06 22:37:15 +0900

    Disable PSR as it seems not to work
    dmesg error:
    [drm:intel_pipe_update_start [i915]] *ERROR* Potential atomic update failure on pipe A

--- louise-configuration.nix
+++ louise-configuration.nix
@@ -19,7 +19,7 @@
 
   boot.extraModprobeConfig = ''
     # See https://wiki.archlinux.org/index.php/Dell_XPS_13_(9360)#Module-based_Powersaving_Options
-    options i915 modeset=1 enable_rc6=1 enable_fbc=1 enable_guc_loading=1 enable_guc_submission=1 enable_psr=1 disable_power_well=0
+    options i915 modeset=1 enable_rc6=1 enable_guc_loading=1 enable_guc_submission=1 enable_psr=0
   '';
 
   boot.blacklistedKernelModules = [
