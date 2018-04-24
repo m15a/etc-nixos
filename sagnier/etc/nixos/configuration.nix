4571ae6e4bd47a6cbb37d9dcf4dcf1518d3d048d
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2018-04-24 14:21:49 +0900

    Do not import a non-existing file

--- sagnier/etc/nixos/configuration.nix
+++ sagnier/etc/nixos/configuration.nix
@@ -14,7 +14,6 @@ in
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./system-specific.nix
     ];
 
   fileSystems."/".options = commonMountOptions;
