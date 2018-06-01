b25a786d221f732a164bcdbb371c3156899be68e
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2018-06-01 12:30:38 +0900

    system.stateVersion -> system.nixos.stateVersion

--- sagnier/etc/nixos/configuration.nix
+++ sagnier/etc/nixos/configuration.nix
@@ -261,6 +261,6 @@ in
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.stateVersion = "18.03"; # Did you read the comment?
+  system.nixos.stateVersion = "18.03"; # Did you read the comment?
 
 }
