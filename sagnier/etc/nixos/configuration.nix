d8705963002d299e8e8367730a48653dee3884f1
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2018-06-17 01:14:29 +0900

    Add btops

--- sagnier/etc/nixos/configuration.nix
+++ sagnier/etc/nixos/configuration.nix
@@ -104,6 +104,7 @@ in
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
+    btops
     dunst
     feh
     scrot
