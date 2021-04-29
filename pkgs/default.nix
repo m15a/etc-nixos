7e900f9f4f30d68a635eaca00074366858b43da5
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-04-29 18:25:29 +0900

    rounded-mgenplus: remove

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -21,20 +21,6 @@ self: super:
     pulseSupport = true;
   };
 
-  rounded-mgenplus = with self;
-  let
-    pname = "rounded-mgenplus";
-    version = "20150602";
-  in fetchzip rec {
-    name = "${pname}-${version}";
-    url = "https://osdn.jp/downloads/users/8/8598/${name}.7z";
-    postFetch = ''
-      ${libarchive}/bin/bsdtar x $downloadedFile
-      install -m 444 -D -t $out/share/fonts/${pname} ${pname}-*.ttf
-    '';
-    sha256 = "0vwdknagdrl5dqwpb1x5lxkbfgvbx8dpg7cb6yamgz71831l05v1";
-  };
-
   shippori = self.callPackage ./shippori {};
 
   yabar-unstable = super.yabar-unstable.overrideAttrs (old: rec {
