842038046e7eb7ed43474b7e2f852442a4357c14
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-05-03 19:34:11 +0900

    Remove obsolete adapta-gtk-theme packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,19 +2,6 @@
 
 self: super:
 {
-  adapta-gtk-theme-colorpack = self.callPackage ./adapta-gtk-theme-colorpack {};
-
-  adapta-gtk-theme-custom = let
-    inherit (config.environment) colors;
-  in with colors.hex;
-  self.callPackage ./adapta-gtk-theme/custom.nix {
-    selectionColor = brorange;
-    accentColor = brgreen;
-    suggestionColor = brorange;
-    destructionColor = red;
-    enableParallel = true;
-  };
-
   oomox-default-theme = self.callPackage ./oomox-default/theme.nix { inherit config; };
   oomox-default-icons = self.callPackage ./oomox-default/icons.nix { inherit config; };
 
