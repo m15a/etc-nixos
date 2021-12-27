e6b8677f921f5a6924587faeb93e0dfbb9109985
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-12-27 16:23:23 +0900

    chore: rename self,super -> final,prev

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,57 +1,57 @@
 { config, ... }:
 
-self: super:
+final: prev:
 
 {
-  oomox-default-theme = self.callPackage ./oomox-default/theme.nix { inherit config; };
-  oomox-default-icons = self.callPackage ./oomox-default/icons.nix { inherit config; };
+  oomox-default-theme = final.callPackage ./oomox-default/theme.nix { inherit config; };
+  oomox-default-icons = final.callPackage ./oomox-default/icons.nix { inherit config; };
 
-  desktop-background = self.runCommand "desktop-background" {} ''
+  desktop-background = final.runCommand "desktop-background" {} ''
     cp ${../data/pixmaps/desktop_background.jpg} $out
   '';
 
-  polybar = super.polybar.override {
+  polybar = prev.polybar.override {
     pulseSupport = true;
   };
 
   configFiles = {
-    bspwm = self.callPackage ./bspwm/config.nix { inherit config; };
+    bspwm = final.callPackage ./bspwm/config.nix { inherit config; };
 
-    fish = self.callPackage ./fish/config.nix { inherit config; };
+    fish = final.callPackage ./fish/config.nix { inherit config; };
 
-    conky = self.callPackage ./conky/config.nix { inherit config; };
+    conky = final.callPackage ./conky/config.nix { inherit config; };
 
-    dunst = self.callPackage ./dunst/config.nix { inherit config; };
+    dunst = final.callPackage ./dunst/config.nix { inherit config; };
 
-    gtk3 = self.callPackage ./gtk3/config.nix { inherit config; };
+    gtk3 = final.callPackage ./gtk3/config.nix { inherit config; };
 
-    sxhkd = self.callPackage ./sxhkd/config.nix { inherit config; };
+    sxhkd = final.callPackage ./sxhkd/config.nix { inherit config; };
 
-    alacritty = self.callPackage ./alacritty/config.nix { inherit config; };
+    alacritty = final.callPackage ./alacritty/config.nix { inherit config; };
 
-    polybar = self.callPackage ./polybar/config.nix {
+    polybar = final.callPackage ./polybar/config.nix {
       inherit config;
-      terminal = "${self.wrapped.alacritty}/bin/alacritty";
+      terminal = "${final.wrapped.alacritty}/bin/alacritty";
     };
   };
 
-  rofi = super.rofi.overrideAttrs (old: {
+  rofi = prev.rofi.overrideAttrs (old: {
     passthru = (old.passthru or {}) // rec {
-      configFile = self.callPackage ./rofi/config.nix {
+      configFile = final.callPackage ./rofi/config.nix {
         inherit config;
-        terminal = "${self.wrapped.alacritty}/bin/alacritty";
+        terminal = "${final.wrapped.alacritty}/bin/alacritty";
       };
-      withConfig = self.callPackage ./rofi/wrapper.nix {
+      withConfig = final.callPackage ./rofi/wrapper.nix {
         inherit configFile;
       };
     };
   });
 
   wrapped = {
-    feh = self.callPackage ./feh/wrapper.nix { inherit config; };
+    feh = final.callPackage ./feh/wrapper.nix { inherit config; };
 
-    alacritty = self.callPackage ./alacritty/wrapper.nix {
-      configFile = self.configFiles.alacritty;
+    alacritty = final.callPackage ./alacritty/wrapper.nix {
+      configFile = final.configFiles.alacritty;
     };
   };
 }
