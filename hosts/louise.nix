3a9cbe5b78378e382e5c265033c723395f764eb0
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-12-28 14:30:33 +0900

    fix(louse): update according to Arch wiki

--- hosts/louise.nix
+++ hosts/louise.nix
@@ -12,22 +12,28 @@
 
   boot = {
     kernelParams = [
-      # https://gist.github.com/greigdp/bb70fbc331a0aaf447c2d38eacb85b8f#sleep-mode-power-usage
+      # Enter S3 deep sleep mode: https://wiki.archlinux.org/title/Dell_XPS_13_(9370)#Sleep
       "mem_sleep_default=deep"
-      # DMAR: DRHD: handling fault status reg 2
-      # https://bbs.archlinux.org/viewtopic.php?id=230362
-      "intel_iommu=off"
+      # DMAR: DRHD: handling fault status reg 2:
+      # https://wiki.archlinux.org/title/Intel_graphics#No_sound_through_HDMI_on_a_Haswell_CPU
+      "intel_iommu=on,igfx_off"
+      # Framebuffer compression (FBC) can reduce power consumption while reducing memory bandwidth
+      # needed for screen refreshes:
+      # https://wiki.archlinux.org/title/intel_graphics#Framebuffer_compression_(enable_fbc)
+      "i915.enable_fbc=1"
+      # Panel Self Refresh (PSR), a power saving feature used by Intel iGPUs is known to cause
+      # flickering in some instances:
+      # https://wiki.archlinux.org/title/intel_graphics#Screen_flickering
+      "i915.enable_psr=0"
+      # Enable both GuC and HuC firmware loading (for processors before Gen 11)
+      # https://wiki.archlinux.org/title/Intel_graphics#Enable_GuC_/
+      "i915.enable_guc=2"
     ];
 
     blacklistedKernelModules = [
       # https://wiki.archlinux.org/index.php/Dell_XPS_13_(9360)#Remove_psmouse_errors_from_dmesg
       "psmouse"
     ];
-
-    extraModprobeConfig = ''
-      # https://wiki.archlinux.org/index.php/Dell_XPS_13_(9360)#Module-based_Powersaving_Options
-      options i915 modeset=1 enable_fbc=1 enable_guc=3 enable_psr=0
-    '';
 
     kernel.sysctl."vm.swappiness" = 10;
   };
@@ -57,6 +63,8 @@
     autorandr.enable = true;
 
     thermald.enable = true;
+
+    throttled.enable = true;
 
     tlp.enable = true;
     tlp.settings = {
