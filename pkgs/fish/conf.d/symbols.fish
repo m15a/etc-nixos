10c4e5257bb95c1404bdb183eb8fef7a816395a4
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2023-12-11 16:11:14 +0900

    chore(fish): show git branch icon

--- pkgs/fish/conf.d/symbols.fish
+++ pkgs/fish/conf.d/symbols.fish
@@ -1,7 +1,7 @@
 set -q __etc_fish_conf_d_symbols_sourced
 or if status is-login
    set -U my_fish_symbol_prompt ❯
-   set -U my_fish_symbol_git ""
+   set -U my_fish_symbol_git_branch "󰘬 "
    set -U my_fish_symbol_nix " "
    set -U my_fish_symbol_python " "
 end
