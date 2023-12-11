bdc27792e1efc76a4fc27ba837f228a7a6c3c69b
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2023-12-11 13:42:43 +0900

    chore: rename myself in description

--- users/mnacamura.nix
+++ users/mnacamura.nix
@@ -2,17 +2,12 @@
 
 {
   users.users.mnacamura = {
-    description = "Mitsuhiro Nakamura";
-
+    description = "NACAMURA Mitsuhiro";
     uid = 1000;
-
     isNormalUser = true;
-
     createHome = true;
-
     extraGroups = [ "wheel" ]
     ++ lib.optionals config.networking.networkmanager.enable [ "networkmanager" ];
-
     shell = "${pkgs.fish}/bin/fish";
   };
 }
