ba34d84918126313eae2e56441f8cd810e669f09
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2023-12-15 15:41:58 +0900

    fix(bspwm): don't use DSLs

--- pkgs/bspwm/config.nix
+++ pkgs/bspwm/config.nix
@@ -3,10 +3,6 @@
 let
   inherit (config.hardware.video.legacy) hidpi;
   inherit (config.environment) colors;
-
-  makeRules = rules:
-  lib.concatStringsSep "\n"
-  (lib.mapAttrsToList (name: rule: "bspc rule --add ${name} ${rule}") rules);
 in
 
 substituteAll (colors.hex // {
@@ -14,19 +10,8 @@ substituteAll (colors.hex // {
 
   postInstall = "chmod +x $out";
 
-  border_width = toString (3 * hidpi.scale);
-
-  window_gap = toString (60 * hidpi.scale);
-
   # 37 is derived from [bspwm window gap: 60] / [phi: 1.618]
   monocle_padding = toString (37 * hidpi.scale);
-
-  rules = makeRules {
-    firefox-nightly = "state=tiled";
-    Steam = "follow=no";
-    Zathura = "state=tiled";
-    bluetoothctl = "state=floating";
-    nmtui = "state=floating";
-    Pavucontrol = "state=floating";
-  };
+  window_gap = toString (60 * hidpi.scale);
+  border_width = toString (3 * hidpi.scale);
 })
