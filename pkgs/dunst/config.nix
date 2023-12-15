d66753175bcb1dd39bd694cc8eae7a39cae7b8c2
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2023-12-15 14:49:07 +0900

    fix(dunst): cannot auto-detect icon path
    Though I find in dunstrc:
    
        # Paths to default icons (only neccesary when not using recursive icon lookup)
        icon_path = /usr/share/icons/gnome/16x16/status/:/usr/share/icons/gnome/16x16/devices/

--- pkgs/dunst/config.nix
+++ pkgs/dunst/config.nix
@@ -5,8 +5,15 @@ let
   inherit (config.environment) colors;
 in
 
-substituteAll (colors.hex // {
+substituteAll (colors.hex // rec {
   src = ./dunstrc;
+
+  icon_theme = "oomox-default";
+  icon_path = let
+    s = toString (24 * hidpi.scale);
+    path = "/run/current-system/sw/share/icons/${icon_theme}";
+  in lib.concatStringsSep ":"
+  (map (c: "${path}/${s}x${s}/${c}") [ "status" "devices" "apps" ]);
 
   browser = "${xdg_utils}/bin/xdg-open";
 })
