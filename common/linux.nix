f60fb898968936e784ce9b1164551a434a284a7f
NAKAMURA Mitsuhiro <m.nacamura@gmail.com>
2024-01-06 00:35:29 +0900

    feat: use docker service

--- common/linux.nix
+++ common/linux.nix
@@ -420,6 +420,12 @@
     };
   };
 
+  virtualisation = {
+    docker.storageDriver = "btrfs";
+    docker.rootless.enable = true;
+    docker.rootless.setSocketVariable = true;
+  };
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
--- users/mnacamura.nix
+++ users/mnacamura.nix
@@ -7,6 +7,7 @@
     isNormalUser = true;
     createHome = true;
     extraGroups = [ "wheel" ]
+    ++ lib.optionals config.virtualisation.docker.enable [ "docker" ]
     ++ lib.optionals config.networking.networkmanager.enable [ "networkmanager" ];
     shell = "${pkgs.fish}/bin/fish";
   };
